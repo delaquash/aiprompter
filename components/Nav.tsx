ee6c0b0833d6b1252897548cbdbb50b80df71fe7
delaquash <olaide1191@gmail.com>
2023-05-14 23:22:26 +0100

    fixed: provider auth call

---
+++ components/Nav.tsx
@@ -0,0 +1,76 @@
+"use client"
+import { signOut, signIn, getProviders, useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+interface IProvider {
+    id?: any ;
+    name?: any;
+}
+const Nav = () => {
+    const isUserLoggedIn = true;
+
+    const [ providers, setProviders ]= useState<IProvider>()
+    
+    useEffect(()=> {
+        const setProvider = async ()=> {
+            const response:any = await getProviders();
+            setProviders(response)
+        }
+        setProvider()
+    }, [])
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+        <Link href="/" className="flex flex-center gap-2">
+            <Image 
+                src="/assets/images/logo.svg"
+                alt="Logo"
+                height={30}
+                width={30}
+                className="object-contain"            
+            />
+            <p className="logo_text">Promptopia</p>
+        </Link>
+        {/* desktop nav */}
+        <div className="sm:flex hidden">
+            {isUserLoggedIn ? (
+                <div className="flex gap-3 md:gap-5">
+                    <Link href="/create-prompt" className="black_btn">
+                        Create Post
+                    </Link>
+                    <button type="button" onClick={signOut} className="outline_btn">
+                        Sign Out
+                    </button>
+                    <Link href="/profile">
+                        <Image
+                            src="/public/assets/images/logo.svg"
+                            height={37}
+                            width={37}
+                            alt="Profile"
+                        />
+                    </Link>
+                </div>
+            ): (
+                <>
+                    {providers && 
+                        Object.values(providers).map((provider)=>(
+                            <button
+                                type="button"
+                                key={provider.name}
+                                onClick={()=>signIn(provider.id)}
+                                className="black_btn"
+                            >
+
+                            </button>
+                        ))
+                    }
+                </>
+            )}
+        </div>
+    </nav>
+  )
+}
+
+export default Nav
