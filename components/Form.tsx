bfefc22089113620952ebbd94dbc9d178acda5d5
delaquash <olaide1191@gmail.com>
2023-05-30 16:59:47 +0100

    fixed: form component

--- components/Form.tsx
+++ components/Form.tsx
@@ -9,6 +9,10 @@ interface IProps {
   handleSubmit?: React.MouseEventHandler
 }
 
+const handleOnchange = (e: { target: { value: string; }}) => {
+  setPost({...post, prompt: e.target.value})
+}
+
 const Form = ({ type, post, setPost, submitting, handleSubmit }: IProps) => {
   return (
     <section className='w-full max-w-full flex-start flex-col'>
@@ -22,17 +26,25 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }: IProps) => {
             className='mt-10 w-full flex flex-col gap-7 glassmorphism max-w-2xl'
           >
             <label>
-              <span className='font-satoshi font-semibold text-base text-gray-700'>Your AI Prompt </span>
+              <span className='font-satoshi font-semibold text-base text-gray-700'>
+                Your AI Prompt
+               </span>
                 <textarea 
                     value={post.prompt}
-                    onChange={(e) => setPost({...post, prompt: e.target.value})}
-
+                    onChange={handleOnchange}
+                    placeholder='Write Your Prompt Here...'
+                    required
+                    className='form_input'
                 />
-
             </label>
+          
           </form>
     </section>
   )
 }
 
 export default Form
+
+// function setPost(arg0: any) {
+//   throw new Error('Function not implemented.');
+// }
