d7caee6d9465b2aca45aa80c27334a54b2fac37b
delaquash <olaide1191@gmail.com>
2023-06-01 11:07:57 +0100

    fixed: route for creating new prompt

--- app/api/prompt/new/route.ts
+++ app/api/prompt/new/route.ts
@@ -1,6 +1,21 @@
+import Prompt from "@models/prompt";
 import { connectedDB } from "@utils/database";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest } from "next/server";
 
-export const POST =async(req: NextApiRequest, res: NextApiResponse)=> {
-    const { userId, prompt, tag }  =await res.json()
+
+export const POST = async (req: NextRequest)=> {
+    const { userId, prompt, tag }  =await req.json()
+
+    try {
+        await connectedDB()
+        const newPrompt = new Prompt({
+            creator:userId,
+            tag,
+            prompt
+        })
+        await newPrompt.save()
+        return new Response(JSON.stringify(newPrompt), {status: 201 })
+    } catch (error) {
+        return new Response("Faled to create a new prompt...")
+    }
 }
