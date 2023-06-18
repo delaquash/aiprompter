6a5ef43892cdf489ee454ae9a4ee00f74097e303
delaquash <olaide1191@gmail.com>
2023-06-18 16:25:03 +0100

    fixed: object value key pairs in carDetails page

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -22,7 +22,8 @@ const CarDetails = ({ isOpen, closeModal, car }:CarDetailsProps) => {
                 </Transition.Child>
                 
                 <div className="fixed inset-0 overflow-y-auto">
-                    <div className="flex min-h-full items-center justify-center p-4 text-center">
+                    <div className="flex min-h-full items-center 
+                        justify-center p-4 text-center">
                         <Transition.Child
                             as={Fragment}
                             enter="ease-out duration-300"
@@ -32,7 +33,55 @@ const CarDetails = ({ isOpen, closeModal, car }:CarDetailsProps) => {
                             leaveFrom='opacity-100 scale-100'
                             leaveTo='opacity-0 scale-95'
                         >
-                            
+                            <Dialog.Panel className="relative w-full max-w-lg 
+                                max-h-[90vh] overflow-y-auto transform rounded-2xl
+                                bg-white text-left shadow-xl transition-all 
+                                p-6    flex flex-col gap-5">
+                                <button
+                                    type='button'
+                                    onClick={closeModal}
+                                    className='absolute top-2 right-2 z-10 w-fit p-2
+                                     bg-primary-blue-100 rounded-full'
+                                >
+                                    <Image 
+                                        src="/close.svg"
+                                        alt='close'
+                                        width={20}
+                                        height={20}
+                                        className='object-contain'
+                                    />
+                                </button>
+                                <div className="flex flex-1 flex-col gap-3">
+                                    <div className="relative w-full h-40 bg-pattern bg-cover
+                                     bg-center rounded-lg">
+                                        <Image src="/hero.png" alt='Car Model' fill priority className='object-contain' />
+                                    </div>
+                                    <div className="flex gap-3">
+                                        <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                                            <Image src="/hero.png" alt='Car' fill priority className='object-contain' />
+                                        </div>
+                                        <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                                            <Image src="/hero.png" alt='Car' fill priority className='object-contain' />
+                                        </div>
+                                        <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                                            <Image src="/hero.png" alt='Car' fill priority className='object-contain' />
+                                        </div>
+                                    </div>
+                                </div>   
+                                <div className="flex-1 flex flex-col gap-2">
+                                    <h2 className="font-semibold text-xl capitalize">
+                                        {car.make} {car.model}
+                                    </h2>
+                                    <div className="mt-3 flex flex-wrap gap-4">
+                                        {Object.entries(car).map(([key, value]: any)=> (
+                                            <div key={key} className="flex justify-between gap-5 w-full text-right">
+                                                <h4 className='text-grey capitalize'>{key.split("_").join(" ")}</h4>
+                                                <p>{value}</p>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>    
+                            </Dialog.Panel>
                         </Transition.Child>
                     </div>
                 </div>
